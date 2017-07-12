9e5e268ff53344694a1738f3ba514b4b4466911c
Matt McDonald <mattmcd1221@gmail.com>
2017-07-12 12:59:02 -0400

    change port

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ app.use(routes);
 
 
 
-app.listen(process.env.PORT || 5000)
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 
